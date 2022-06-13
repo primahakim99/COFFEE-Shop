4d159140ef9a9e2ed82900f1833edf8313fa3dea
primahakim99 <fahreza.prima@gmail.com>
2022-06-14 06:15:25 +0700

    model admin, owner, user

---
+++ app/Models/Store.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'storeName',
+        'address',
+    ];
+
+    public function owner(){
+        return $this->hasOne(User::class);
+    }
+}
