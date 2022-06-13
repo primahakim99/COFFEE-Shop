d360418c413df43e05aa695f285244236f552734
primahakim99 <fahreza.prima@gmail.com>
2022-06-14 06:16:20 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +16,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+Route::get('/shop', [WelcomeController::class, 'shop'])->name('shop');
+Route::get('/wishlist', [WelcomeController::class, 'wishlist'])->name('wishlist');
+Route::get('/cart', [WelcomeController::class, 'cart'])->name('cart');
+Route::get('/checkout', [WelcomeController::class, 'checkout'])->name('checkout');
+
+Route::get('/sign-in', [WelcomeController::class, 'SignIn'])->name('auth.sign-in');
+Route::get('/sign-up', [WelcomeController::class, 'SignUp'])->name('auth.sign-up');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'OwnerAccess'])->group(function () {
+    // Route::get('/owner', [HomeController::class, 'ownerHome'])->name('owner.home');
+    Route::get('/owner', [WelcomeController::class, 'indexOwner'])->name('welcomeOwner');
+});
+
+Route::middleware(['auth', 'AdminAccess'])->group(function () {
+    // Route::get('/admin', [HomeController::class, 'adminHome'])->name('admin.home');
+    Route::get('/admin', [WelcomeController::class, 'indexAdmin'])->name('welcomeAdmin');
+});
+
+Route::get('/customer/home', [HomeController::class, 'customerHome'])->name('customer.home')->middleware('UserAccess');
+
+
+//admin side
+Route::get('/ownerData', [WelcomeController::class, 'ownerData'])->name('ownerData');
+Route::get('/customerData', [WelcomeController::class, 'customerData'])->name('customerData');
+Route::get('/transaction', [WelcomeController::class, 'transaction'])->name('transaction');
+
+//owner side
+Route::get('/productList', [WelcomeController::class, 'product_list'])->name('productList');
+Route::get('/storeOwner', [WelcomeController::class, 'storeOwner'])->name('storeOwner');
+Route::get('/orderOwner', [WelcomeController::class, 'orderOwner'])->name('orderOwner');
